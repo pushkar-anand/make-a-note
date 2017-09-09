c9a00e86cd6fb3200db9e6c71bff216825902eed
Pushkar Anand <anandpushkar088@gmail.com>
2017-09-10 04:39:37 +0530

    Deleted test file

--- test.py
+++
@@ -1,9 +0,0 @@
-import json
-if __name__ == '__main__':
-
-    with open('notes.json') as data_file:
-        data = json.load(data_file)
-    notes = data["notes"]
-    print(notes)
-    #notes_data = json.loads(notes[0])
-    print(notes[0])
